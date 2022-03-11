d212e95d36c616bd61ff646b0b229b613b91b138
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-03-11 18:10:16 +0800

    0002: rewrite unittest method
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

--- src/0002/0002-Add-Two-Numbers-UnitTest.cpp
+++ src/0002/0002-Add-Two-Numbers-UnitTest.cpp
@@ -2,17 +2,21 @@
 
 #include "0002-Add-Two-Numbers.cpp"
 
-TEST(AddTwoNumbersTest, Solution1) {
-  auto addTwoNumbers = [](const std::string& val1,
-                          const std::string& val2) -> std::string {
-    Solution1 s;
-    return ToString(s.addTwoNumbers(ToListNode(val1), ToListNode(val2)));
-  };
+#define LEETCODE_TEST(SolutionX)                                            \
+  TEST(AddTwoNumbersTest, SolutionX) {                                      \
+    auto addTwoNumbers = [](const std::string& val1,                        \
+                            const std::string& val2) -> std::string {       \
+      SolutionX s;                                                          \
+      return ToString(s.addTwoNumbers(ToListNode(val1), ToListNode(val2))); \
+    };                                                                      \
+                                                                            \
+    EXPECT_EQ("[]", addTwoNumbers("[]", "[]"));                             \
+    EXPECT_EQ("[1]", addTwoNumbers("[1]", "[0]"));                          \
+    EXPECT_EQ("[5]", addTwoNumbers("[1]", "[4]"));                          \
+    EXPECT_EQ("[5,6]", addTwoNumbers("[4,1]", "[1,5]"));                    \
+    EXPECT_EQ("[7,0,8]", addTwoNumbers("[2,4,3]", "[5,6,4]"));              \
+    EXPECT_EQ("[8,9,9,9,0,0,0,1]",                                          \
+              addTwoNumbers("[9,9,9,9,9,9,9]", "[9,9,9,9]"));               \
+  }
 
-  EXPECT_EQ("[]", addTwoNumbers("[]", "[]"));
-  EXPECT_EQ("[1]", addTwoNumbers("[1]", "[0]"));
-  EXPECT_EQ("[5]", addTwoNumbers("[1]", "[4]"));
-  EXPECT_EQ("[5,6]", addTwoNumbers("[4,1]", "[1,5]"));
-  EXPECT_EQ("[7,0,8]", addTwoNumbers("[2,4,3]", "[5,6,4]"));
-  EXPECT_EQ("[8,9,9,9,0,0,0,1]", addTwoNumbers("[9,9,9,9,9,9,9]", "[9,9,9,9]"));
-}
+LEETCODE_TEST(Solution1)
