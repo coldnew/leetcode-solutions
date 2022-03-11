3e3a3f9404a6013f9fe65dbf46ef65706e9f85e8
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-03-11 18:11:32 +0800

    0445: rewrite unittest method
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

--- src/0445/0445-Add-Two-Numbers-II-UnitTest.cpp
+++ src/0445/0445-Add-Two-Numbers-II-UnitTest.cpp
@@ -14,15 +14,18 @@ TEST(AddTwoNumbersIITest, ReverseList) {
   EXPECT_EQ("[1,2,3,4,5,6,7,8,9]", ReverseList("[9,8,7,6,5,4,3,2,1]"));
 }
 
-TEST(AddTwoNumbersIITest, Solution1) {
-  auto addTwoNumbers = [](const std::string& val1,
-                          const std::string& val2) -> std::string {
-    Solution1 s;
-    return ToString(s.addTwoNumbers(ToListNode(val1), ToListNode(val2)));
-  };
+#define LEETCODE_TEST(SolutionX)                                            \
+  TEST(AddTwoNumbersIITest, SolutionX) {                                    \
+    auto addTwoNumbers = [](const std::string& val1,                        \
+                            const std::string& val2) -> std::string {       \
+      SolutionX s;                                                          \
+      return ToString(s.addTwoNumbers(ToListNode(val1), ToListNode(val2))); \
+    };                                                                      \
+                                                                            \
+    EXPECT_EQ("[]", addTwoNumbers("[]", "[]"));                             \
+    EXPECT_EQ("[0]", addTwoNumbers("[0]", "[0]"));                          \
+    EXPECT_EQ("[8,0,7]", addTwoNumbers("[2,4,3]", "[5,6,4]"));              \
+    EXPECT_EQ("[7,8,0,7]", addTwoNumbers("[7,2,4,3]", "[5,6,4]"));          \
+  }
 
-  EXPECT_EQ("[]", addTwoNumbers("[]", "[]"));
-  EXPECT_EQ("[0]", addTwoNumbers("[0]", "[0]"));
-  EXPECT_EQ("[8,0,7]", addTwoNumbers("[2,4,3]", "[5,6,4]"));
-  EXPECT_EQ("[7,8,0,7]", addTwoNumbers("[7,2,4,3]", "[5,6,4]"));
-}
+LEETCODE_TEST(Solution1)
